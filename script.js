3f1bb3750db8a5c6bf24d87bf3fd449fc1c274bc
Anjal Sali <anjalsali55@gmail.com>
2023-12-12 17:25:42 +0000

    added codes to load and save events to local storage

--- script.js
+++ script.js
@@ -3,10 +3,38 @@ $(document).ready(function () {
    function updateCalendar() {
       var currentDay = dayjs().format("dddd, MMMM D, YYYY");
       $("#currentDay").text(currentDay);
+
+      var currentTime = dayjs().hour();
+      $(".time-block").each(function () {
+         var blockHour = parseInt($(this).attr("data-hour"));
+
+         // Update the classes based on past, present, or future
+         if (blockHour < currentTime) {
+            $(this).removeClass("present future").addClass("past");
+         } else if (blockHour === currentTime) {
+            $(this).removeClass("past future").addClass("present");
+         } else {
+            $(this).removeClass("past present").addClass("future");
+         }
+      });
+   }
+
+   // Function to load events from local storage
+   function loadEvents() {
+      $(".time-block").each(function () {
+         var blockHour = $(this).attr("data-hour");
+         var eventText = localStorage.getItem(blockHour);
+         $(this).find("textarea").val(eventText);
+      });
+   }
+
+   // Function to save events to local storage
+   function saveEvent(hour, text) {
+      localStorage.setItem(hour, text);
    }
 
    updateCalendar();
-
+   loadEvents();
    // Update the calendar every minute
    setInterval(function () {
       updateCalendar();
